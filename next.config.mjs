dab52f219f7ba7022991d1a58f9943769e438bb1
Rashid Shamloo <rashidshamloo@gmail.com>
2023-08-11 02:31:28 +0330

    trying a fix for vercel build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
   webpack: (config) => {
     config.resolve.alias['@'] = __dirname;
     return config;
